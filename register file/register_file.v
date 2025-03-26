c4dedab1f9d63196f4923e008b3bcf71ca419bf0
Seohyeon Kim <112575818+seohyeonKim111@users.noreply.github.com>
2025-03-26 17:18:20 -0400

    Update register_file.v

--- register file/register_file.v
+++ register file/register_file.v
@@ -1,12 +1,33 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/26/2025 05:17:33 PM
+// Design Name: 
+// Module Name: register_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module register_file (
     input  [3:0]  RegRead1, 
     input  [3:0]  RegRead2,
     input  [3:0]  WriteReg,
     input  [15:0] WriteData,
     input         RegWrite,
-    output reg [15:0] ReadData1,
-    output reg [15:0] ReadData2
+    output [15:0] ReadData1,
+    output [15:0] ReadData2
 );
     
     reg [15:0] registers [0:15];
