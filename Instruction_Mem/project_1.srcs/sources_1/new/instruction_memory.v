0c762d62490926d04622b02fa64267a7dd6e6f8b
MatthewLiddiard <143458579+MatthewLiddiard@users.noreply.github.com>
2025-03-29 17:12:49 -0400

    Update instruction_memory.v
    I suck

--- Instruction_Mem/project_1.srcs/sources_1/new/instruction_memory.v
+++ Instruction_Mem/project_1.srcs/sources_1/new/instruction_memory.v
@@ -4,28 +4,43 @@
 module instruction_memory(
     
     input [15:0] address,        // Address of the instruction to fetch
-    output wire [15:0] instruction, // 16-bit instruction fetched from memory
+    output reg [15:0] instruction, // 16-bit instruction fetched from memory
     input clk
 
     );
     // Memory array for 64 locations, each 16 bits wide
     reg [15:0] memory [63:0];
     reg [15:0] OutputReg;  // Internal reg to hold the output value
-    reg [3:0] opcode;
-    integer i = 1;
+    
+   //addi r1 1
+   //add r2 2
+   //add r3 r2
+   //sub r3 r1
    
+   
+   initial begin
+        memory[0] = 16'b0011000100010001;  // addi r1 r1 1
+        memory[1] = 16'b0011001000100010;  // addi r2 r2 2
+        
+        memory[2] = 16'b0000001100100000;  // Example instruction (add) r3 r2
+        memory[3] = 16'b0000001100010001;  // Example instruction (add)
+        
+        //memory[2] = 16'b0110000000000001;  // jmp
+        // More instructions can be loaded here
+    end
+    
     
     assign OutputData = instruction;
     
     always @ (posedge clk) begin
-    if (i == 1) begin
-        $readmemb("./memfile.txt",memory,0,4);
-        i = 0;
-    end
     
-    OutputReg <= memory[address];
+    
+    instruction <= memory[address];
     
     end
+    
+   // Monitor important signals (optional)
+  
     
     
 endmodule
