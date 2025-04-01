bf32828dd4d15791a2bf14c3579ca4ed635b26ed
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2025-04-01 18:09:17 -0400

    Update 2:1mux.v

--- mux/2:1mux.v
+++ mux/2:1mux.v
@@ -1,25 +1,26 @@
-//iverilog -o mux.vvp mux.v mux_tb.v
-//vvp mux.vvp
-
 module mux(
     input [15:0] I0,        //register data 2
     input [15:0] I1,        //sign extended value
     input Selector,
+    input clk,
     output reg [15:0] Output,
     output temp1,
     output temp2
 );
 
-always @(*) begin
+    
+    always @(*) begin
     case(Selector)
-        0: begin
-            Output = I0;
+        1'b0: begin
+        Output = I0;
         end
-
-        1: begin
-            Output = I1;
+        
+        1'b1: begin
+        Output = I1;
         end
-    endcase
+        
+        endcase
+    
+    end
 
-end
 endmodule
