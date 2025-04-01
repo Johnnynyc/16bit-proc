fb0129242bc5b80354fbcbb634cd37d418c46de3
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2025-04-01 18:08:18 -0400

    Update control_unit.v

--- Control Unit/control_unit.v
+++ Control Unit/control_unit.v
@@ -1,29 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/26/2025 05:15:05 PM
-// Design Name: 
-// Module Name: control_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module control_unit(clk,opcode,function_code,RegDst,RegWrite, Branch,Jump,ALUop, MemRead,MemWrite,RegWriteSource,ALUSource);
-    input clk;    
+module control_unit(opcode,function_code,clk,RegDst,RegWrite, Branch,Jump,ALUop, MemRead,MemWrite,RegWriteSource,ALUSource);
     input [3:0] opcode;
     input [3:0] function_code;
+    input clk;
     output reg RegDst;
     output reg RegWrite;
     output reg Branch;
@@ -33,7 +13,8 @@ module control_unit(clk,opcode,function_code,RegDst,RegWrite, Branch,Jump,ALUop,
     output reg MemWrite;
     output reg RegWriteSource;
     output reg ALUSource;
-    
+
+
     initial begin
         RegDst =0;
         RegWrite=0;
@@ -50,51 +31,21 @@ module control_unit(clk,opcode,function_code,RegDst,RegWrite, Branch,Jump,ALUop,
         begin
         case(opcode)
             4'b0000: begin //R-type: add, sub, sll, and
+                RegDst =1;
+                RegWrite =1;
+          
+                Branch = 0;
+                Jump = 0;
+                MemRead = 0;
+                MemWrite = 0;
+                RegWriteSource = 0;
+                ALUSource = 0;
+                
                 case(function_code)
-                    4'b0000: begin
-                        RegDst = 1;       
-                        RegWrite = 1;
-                        Branch = 0;
-                        Jump = 0;
-                        ALUop = 4'b0000;
-                        MemRead = 0;
-                        MemWrite = 0;
-                        RegWriteSource = 0;
-                        ALUSource=0;
-                    end
-                    4'b0001: begin
-                        RegDst = 1;       
-                        RegWrite = 1;
-                        Branch = 0;
-                        Jump = 0;
-                        ALUop = 4'b0001;
-                        MemRead = 0;
-                        MemWrite = 0;
-                        RegWriteSource = 0;
-                        ALUSource=0;
-                    end
-                    4'b0010: begin
-                        RegDst = 1;       
-                        RegWrite = 1;
-                        Branch = 0;
-                        Jump = 0;
-                        ALUop = 4'b0010;
-                        MemRead = 0;
-                        MemWrite = 0;
-                        RegWriteSource = 0;
-                        ALUSource=0;
-                    end
-                    4'b0011: begin
-                        RegDst = 1;       
-                        RegWrite = 1;
-                        Branch = 0;
-                        Jump = 0;
-                        ALUop = 4'b0011;
-                        MemRead = 0;
-                        MemWrite = 0;
-                        RegWriteSource = 0;
-                        ALUSource=0;
-                    end
+                    4'b0000: ALUop = 4'b0000;
+                    4'b0001: ALUop = 4'b0001;
+                    4'b0010: ALUop = 4'b0010;
+                    4'b0011: ALUop = 4'b0011;
                 endcase
             end
             
@@ -164,7 +115,7 @@ module control_unit(clk,opcode,function_code,RegDst,RegWrite, Branch,Jump,ALUop,
                 RegWrite = 0;
                 Branch = 0;
                 Jump = 1;
-                ALUop = 4'b0000; 
+                ALUop = 4'b0000; // May not be used
                 MemRead = 0;
                 MemWrite = 0;
                 RegWriteSource = 0;
